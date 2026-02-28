51a4cbfd57dcaa020658bcb5dd5683d49c9de38a
mlegare <mlegare777@gmail.com>
2026-02-28 10:30:54 -0500

    Install material

--- quests/quest-08/current/curious-academy/apps/ludoteck8/src/app/app.ts
+++ quests/quest-08/current/curious-academy/apps/ludoteck8/src/app/app.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { ListVideoGames } from '../features/video-games/video-games';
+import { MatSliderModule } from '@angular/material/slider';
 
 
 @Component({
-  imports: [ListVideoGames],
+  imports: [ListVideoGames, MatSliderModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'ludoteck8';
+
+
+  formatLabel(value: number): string {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + '000';
+    }
+
+    return `${value}`;
+  }
+
 }
