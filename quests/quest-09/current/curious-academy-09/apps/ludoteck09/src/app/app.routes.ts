a07d32960db563f83170175f64eef9ef1ddfafa1
mlegare <mlegare777@gmail.com>
2026-03-07 13:34:46 -0500

    loadChildren: () => import

--- quests/quest-09/current/curious-academy-09/apps/ludoteck09/src/app/app.routes.ts
+++ quests/quest-09/current/curious-academy-09/apps/ludoteck09/src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import { NotFound } from '../shared/errors/not-found/not-found';
-import { videoGamesRoutes } from '../features/video-games/video-games.routes';
 
 export const appRoutes: Routes = [
 
 
-    //    ...videoGamesRoutes,
+
     {
         path: '',
-        children: videoGamesRoutes
+        redirectTo: 'jeux-videos',
+        pathMatch: 'full',
+    },
+    {
+        path: 'jeux-videos',
+        loadChildren: () => import('../features/video-games/video-games.routes').then(item => item.videoGamesRoutes)
     },
     {
         path: '**',
--- quests/quest-09/current/curious-academy-09/apps/ludoteck09/src/features/video-games/video-games.routes.ts
+++ quests/quest-09/current/curious-academy-09/apps/ludoteck09/src/features/video-games/video-games.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 import { EditVideoGame } from './components/edit-video-game/edit-video-game';
 import { ListVideoGames } from './video-games';
 
+// route actuelle dans cette classe 'jeux-videos'
 export const videoGamesRoutes: Routes = [
     {
-        path: 'jeux-videos',
+        path: '',
         component: ListVideoGames
     },
     {
-        path: 'jeux-videos/:id',
+        path: ':id',
         component: EditVideoGame
     }
 ];
