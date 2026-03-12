ca13bc9c4455ceaf1125e7bf00e448bbb602ddaf
mlegare <mlegare777@gmail.com>
2026-03-12 11:45:29 -0400

    ajout Guard (isAdminGuard)

--- quests/quest-10/current/curious-academy-10/apps/ludoteck10/src/features/statistics/statistics.routes.ts
+++ quests/quest-10/current/curious-academy-10/apps/ludoteck10/src/features/statistics/statistics.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from "@angular/router";
 import { ListStats } from "./components/list-stats/list-stats";
 import { OneStat } from "./components/one-stat/one-stat";
+import { isAdminGuard } from "./guards/is-admin-guard";
 
 export const statisitcsRoutes: Routes = [
     {
         path: '',
-        component: ListStats
+        component: ListStats,
+        canActivate: [isAdminGuard]
     },
     {
         path: 'one-stat',
