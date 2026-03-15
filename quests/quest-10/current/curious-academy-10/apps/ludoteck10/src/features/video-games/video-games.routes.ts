93318d857d5e36292b8a1d54522bbc2fb40d4277
mlegare <mlegare777@gmail.com>
2026-03-15 10:54:46 -0400

    Ajout de title dans les routes 2

--- quests/quest-10/current/curious-academy-10/apps/ludoteck10/src/features/video-games/video-games.routes.ts
+++ quests/quest-10/current/curious-academy-10/apps/ludoteck10/src/features/video-games/video-games.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { EditVideoGame } from './components/edit-video-game/edit-video-game';
 import { ListVideoGames } from './video-games';
+import { getDynamicTitle } from '../../shared/tools/routes/titles/dynamic-title';
 
 // route actuelle dans cette classe 'jeux-videos'
 export const videoGamesRoutes: Routes = [
     {
         path: '',
         component: ListVideoGames
+    },
+    {
+        path: 'title/:id',
+        component: ListVideoGames,
+        title: getDynamicTitle
     },
     {
         path: ':id',
