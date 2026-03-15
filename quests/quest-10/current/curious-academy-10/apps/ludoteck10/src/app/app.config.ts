ab709d559b14575e3b739ba85fab22a9787ecc52
mlegare <mlegare777@gmail.com>
2026-03-15 12:16:30 -0400

    Ajout de logger interceptor

--- quests/quest-10/current/curious-academy-10/apps/ludoteck10/src/app/app.config.ts
+++ quests/quest-10/current/curious-academy-10/apps/ludoteck10/src/app/app.config.ts
@@ -4,12 +4,13 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { loggerInterceptor, loggerWarnInterceptor } from '../shared/tools/routes/titles/logger-intercetpor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([loggerInterceptor, loggerWarnInterceptor])),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes),
-    provideHttpClient()
+    provideRouter(appRoutes)
   ],
 };
