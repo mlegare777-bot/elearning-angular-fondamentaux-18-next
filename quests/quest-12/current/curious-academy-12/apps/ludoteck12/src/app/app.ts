e02ad7fde3751617a5de5b5bce03213f001ddfbd
mlegare <mlegare777@gmail.com>
2026-03-28 13:36:44 -0400

    Function pipe pure=false

--- quests/quest-12/current/curious-academy-12/apps/ludoteck12/src/app/app.ts
+++ quests/quest-12/current/curious-academy-12/apps/ludoteck12/src/app/app.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { RouterOutlet } from '@angular/router';
 import { MainMenu } from '../shared/menus/main-menu/main-menu';
+import { ComputePipe } from '../shared/tools/routes/titles/compute-pipe';
 
 
 @Component({
-  imports: [MatSliderModule, RouterOutlet, MainMenu],
+  imports: [ComputePipe, MatSliderModule, RouterOutlet, MainMenu],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'ludoteck12';
-
+  value = 5;
 
   formatLabel(value: number): string {
     if (value >= 1000) {
